3b14c8979b48a73fa67c9c1a2dc71c95a1d0a250
Mannur <zmannur@gmail.com>
2022-08-16 16:42:45 +0400

    16/08/2022 16:42

--- create_teplate_zip.py
+++ create_teplate_zip.py
@@ -23,41 +23,80 @@ import os
 def input_data():
     """ввод данных для изменения в файле"""
     print('введите текст поиска')
-    # name_org = input()
     d_input, d_replace = {}, {}
 
-    d_input['name_org'] = 'Государственное бюджетное общеобразовательное учреждение ' \
-                          'Самарской области средняя общеобразовательная школа ' \
-                          '«Образовательный  центр имени В.Н. Татищева» с. Челно-Вершины ' \
-                          'муниципального района Челно-Вершинский Самарской области'
-    d_input['name_org_var1'] = 'Государственное бюджетное общеобразовательное учреждение '
-    d_input['name_org_var2'] = 'Государственное бюджетное общеобразовательное учреждение'
-    d_input['name_org_var3'] = 'Государственное бюджетное общеобразовательное учреждение Самарской области'
-    d_input['name_org_var4'] = 'Государственное бюджетное общеобразовательное учреждение Самарской области '
+    d_input['name_org'] = """ Государственное бюджетное общеобразовательное учреждение\
+                          Самарской области средняя общеобразовательная школа \
+                          «Образовательный  центр имени В.Н. Татищева» с. Челно-Вершины \
+                          муниципального района Челно-Вершинский Самарской области"""
+    d_input['name_org_var1'] = 'Самарской области '
+    d_input['name_org_var2'] = 'средняя общеобразовательная школа'
+    d_input['name_org_var3'] = 'муниципального района'
+    d_input['name_org_var4'] = 'Государственное бюджетное'
+    d_input['name_org_var5'] = 'Образовательный  центр '
 
-    d_input['name_prof'] = 'Н.А. Сергеева'
-    d_input['name_director'] = 'Н.В. Моисеева'
-    d_input['name_spec'] = 'Специалист  по охране труда_____________ М.М. Зайдуллин'
+    d_input['name_prof'] = 'Сергеева'
+    d_input['name_director'] = 'Моисеева'
+    d_input['name_spec'] = 'Зайдуллин'
     d_input['name_pos_prof'] = "Председатель профкома"
     d_input['name_pos_direct'] = 'Директор школы'
     d_input['name_prof_var1'] = 'Н.А.Сергеева'
     d_input['name_director_var1'] = 'Н.В.Моисеева'
+    d_input['grif_1'] = 'СОГЛАСОВАНО'
+    d_input['grif_2'] = 'УТВЕРЖДАЮ'
+    d_input['data'] = '25 января 2017 г.'
 
     print('введите текст изменения!')
-    d_replace['name_org'] = '{{name_org}}'
-    d_input['name_org_var1'] = '{{name_org}}'
-    d_input['name_org_var2'] = '{{name_org}}'
-    d_input['name_org_var3'] = '{{name_org}}'
-    d_input['name_org_var4'] = '{{name_org}}'
-    d_replace['name_prof'] = '{{name_prof}}'
-    d_replace['name_director'] = '{{name_director}}'
-    d_replace['name_spec'] = '{{name_spec}}'
-    d_replace['name_pos_prof'] = '{{name_pos_prof}}'
-    d_replace['name_pos_direct'] = '{{name_pos_direct}}'
-    d_replace['name_prof_var1'] = '{{name_prof}}'
-    d_replace['name_director_var1'] = '{{name_director}}'
-
+    d_replace['name_org'] = ''
+    d_replace['name_org_var1'] = ''
+    d_replace['name_org_var3'] = ''
+    d_replace['name_org_var4'] = ''
+    d_replace['name_org_var2'] = ''
+    d_replace['name_org_var5'] = ''
+    d_replace['name_prof'] = ''
+    d_replace['name_director'] = ''
+    d_replace['name_pos_prof'] = ''
+    d_replace['name_pos_direct'] = ''
+    d_replace['name_prof_var1'] = ''
+    d_replace['name_director_var1'] = ''
+    d_replace['name_spec'] = ''
+    d_replace['grif_1'] = ''
+    d_replace['grif_2'] = ''
+    d_replace['data'] = ''
     return d_input, d_replace
+
+
+def del_paragrafs_docx():
+    folder = 'Upload_folder'
+    for file in os.listdir(folder):
+        file_path = os.path.join(os.getcwd(), folder, file)
+        doc = docx.Document(file_path)
+        all_paragrs = doc.paragraphs
+        len_paragrs = len(all_paragrs)
+        print(file)
+
+        count = 1
+        for paragr in all_paragrs:
+            if "№" in paragr.text:
+                break
+            else:
+                count += 1
+        print(count)
+        for paragr in all_paragrs:
+            delete_paragraph(paragr)
+            print(count)
+            if count <= 2:
+                break
+            else:
+                count -= 1
+        doc.save(file_path)
+
+
+def delete_paragraph(paragraph):
+    p = paragraph._element
+    p.getparent().remove(p)
+    p._p = p._element = None
+    return
 
 
 def unzip_archive():
@@ -70,79 +109,46 @@ def unzip_archive():
 
 def replace_data(d_input, d_replace):
     """поиск текста на замену в файле"""
-
-    # в цикле по файлам
-    #   открыть файл , прочитать файл в буфер
-    #   в цикле по условию
-    #      взять первой значение
-    #      найти место замены,
-    #      сделать замену
-    #
-    # сохранить файл
     folder = 'Upload_folder'
-    folder_base = os.getcwd()
-    count = 1
 
-    print('1', os.getcwd())
     for file in os.listdir(folder):
-        os.chdir(folder)
-        os.getcwd()
-        # print(os.getcwd())
-        file_path = os.path.abspath(file)
-        print(file,  file_path)
-
+        file_path = os.path.join(os.getcwd(), folder, file)
         doc = docx.Document(file_path)
-        all_paras = doc.paragraphs
-
+        all_paragrs = doc.paragraphs
         for key, value in d_input.items():
-            # print('key and value---', key, value)
-            for paragr in all_paras:
+            for paragr in all_paragrs:
                 inline = paragr.runs
-                # print(inline, len(inline))
                 if value in paragr.text:
                     print('Sucsess!')
-                    print('OLD', paragr.text)
                     paragr.text = d_replace[key]
-                    print("NEW", paragr.text)
-                # else:
-                #     print(value, '-Fail!')
-                # Loop added to work with runs (strings with same style)
-                # for i in range(len(inline)):
-                #     if 'old text' in inline[i].text:
-                #         text = inline[i].text.replace('old text', 'new text')
-                #         inline[i].text = text
 
 
-                os.chdir(folder_base)
-                # TODO нужны два файла в одной директорри
-                #  иначе постоянно прыгать за файлами в разные директории
-                # TODO нужно очистить тектс из параграфа для возможного сравнения
+            for table in doc.tables:
+                for row in table.rows:
+                    for cell in row.cells:
+                        if value in cell.text:
+                            print('Sucsess! Table!')
+                            cell.text = d_replace[key]
 
-            # for table in doc.tables:
-            #     for cell in table.cells:
-            #         for paragr in cell.paragraphs:
-            #             if value in paragr.text:
-            #                 paragr.text = d_replace[key]
-                        
         doc.save(file_path)
     return
-
-
 
 
 def zip_template():
     path = 'Upload_folder'  # '/home/docs-python/script/sql-script/'
     file_dir = os.listdir(path)
-    with zipfile.ZipFile('_instr_new.zip.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+    with zipfile.ZipFile('_instr_template.zip', mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
         for file in file_dir:
             add_file = os.path.join(path, file)
             zf.write(add_file)
 
 
 def main():
-    # unzip_archive()
+    unzip_archive()
     d_input, d_replace = input_data()
     replace_data(d_input, d_replace)
+    del_paragrafs_docx()
+    zip_template()
 
 
 if __name__ == '__main__':
