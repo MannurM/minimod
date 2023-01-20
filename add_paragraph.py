087d0687a1790baf26328ce6d56ad508985858ac
Mannur <zmannur@gmail.com>
2023-01-20 16:21:55 +0400

    19/01/2023 16:44

--- add_paragraph.py
+++ add_paragraph.py
@@ -21,18 +21,6 @@ def add_path_folder():
         return path_folder
 
 
-def search_replace(doc, search_string, replace_string):
-    pass
-    # doc.add_tables
-
-    # for paragraph in doc.paragraphs:
-    #     if paragraph == replace_string:
-    #         break
-    #     elif paragraph == search_string:
-    #         paragraph = replace_string
-    # return doc
-
-
 def input_data():
     """ввод данных для изменения в файле"""
     print('введите текст поиска')
@@ -81,42 +69,6 @@ def read_files(path_folder):
     return
 
 
-# def create_tepl_file(path_folder):
-#     styles = []
-#     for file_name in os.listdir(path_folder):
-#         if file_name == 'instr_1.docx':
-#             print('first')
-#             doc_1 = docx.Document(path_folder + '\\' + file_name)
-#             for parag in doc_1.paragraphs:
-#                 styles.append(parag.style)
-#                 para_row = parag.runs
-#                 for row in parag.runs:
-#                     para_row = parag.add_run(row.text)
-#                     # Font data
-#                     para_row.style.name = row.style.name
-#                     # Size of font data
-#                     para_row.font.size = row.font.size
-#                     # Bold data
-#                     para_row.bold = row.bold
-#                     # Italic data
-#                     para_row.italic = row.italic
-#                     # Underline data
-#                     para_row.underline = row.underline
-#                     # Color data
-#                     para_row.font.color.rgb = row.font.color.rgb
-#
-#     document = docx.Document()
-#     style = document.styles['Normal']
-#     font = style.font
-#     from docx.shared import Pt
-#     font.name = 'Times New Roman'
-#     font.size = Pt(12)
-#     style = document.styles['Instr_Text']
-#     style.hidden = False
-#     style.quick_style = True
-#     style.priorty = 1
-
-
 def compile_file(path_folder):
     def cell_format():
         cell.paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
@@ -133,6 +85,11 @@ def compile_file(path_folder):
 
         for paragraph in doc.paragraphs:
             p_text = paragraph.text
+            # print(p_text[-1:])
+
+            if p_text[-1:] == '^|':
+                p_text[-1:] = '^p'
+                print('Замена мягкого абзаца')
             if p_text[:10] == 'Инструкция' or p_text[:10] == 'ИНСТРУКЦИЯ':
                 table = doc_new.add_table(rows=4, cols=1)
                 table.alignment = WD_TABLE_ALIGNMENT.LEFT
@@ -183,7 +140,8 @@ def compile_file(path_folder):
                 # para.alignment = 3  # выравниевание по ширине
                 para.paragraph_format.line_spacing = 1.0
                 # TODO  Удалить лишние интервалы?
-                # TODO Как унифицировать псоледнюю строку абзаца чтобы не было большых пробелов на строке
+                # Как унифицировать псоледнюю строку абзаца чтобы не было больших пробелов на строке
+                # заменить разрыв строки на абзац
                 para_row = paragraph.runs
                 for row in para_row:
                     para_row = para.add_run(row.text)
@@ -208,16 +166,14 @@ def compile_file(path_folder):
                                       'XV.', 'XVI']
                 if p_text[:3] in list_format_center:
                     para.alignment = 1  # Центрирование заголовков по центру
-
         doc_new.save('new_' + file_name)
 
 
 def main():
-    print(1)
-    # search_string, replace_string = input_data()
+    print('1')
     path_folder = add_path_folder()
     os.chdir(path_folder)
-    read_files(path_folder)
+    # read_files(path_folder)
     compile_file(path_folder)
 
 
@@ -225,10 +181,5 @@ if __name__ == '__main__':
     print('0')
     main()
 
-
-#  Каждый файл  чтобы добавить таблицу нужно переписать?
-#  сначала создать новый файл
-#  потом вставить в него элементы в той последовательности которой нужно
-#  и сохранить под старым именем?? или новым ??
-# TODO работа со стилями исходного документа или вообще нужен 1 шаблонный документ?
-#  и как привести все к обному виду единообразию
+# TODO Нужно расписать максимальнов возможные ошибки для выявления и коррекции
+# TODO Нужно сделать единый шаблон для инструкции и на основании его привести все  инструкции к единообразию!
