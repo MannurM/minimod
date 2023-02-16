810db8f2bfc30388b82b0fb3fbe81d8c576e18a2
Mannur <zmannur@gmail.com>
2023-02-16 15:53:36 +0400

    16/02/2023 15:53

--- two_space.py
+++ two_space.py
@@ -23,16 +23,44 @@ for file_name in os.listdir(path_folder):
         # run = par.add_run()
         text = par.text
         new_text = text
-
+        if not text:
+            continue
         list_index = []
-        # text = text.lstrip()
-
+        label = 0
         for index, txt in enumerate(text):
             if text[index].isalpha() or text[index].isdigit():
                 # print('exit')
                 break
             elif text[:1] == ' ':
                 text = text.lstrip()
+        text = text.rstrip()
+        if text[-2:] == '\r':
+            text = text.replace('\\r', '')
+        if text[-1:] == '\r':
+            text = text.replace('\r', '')
+
+        print('LF')
         par.text = text
+        count = len(text) + 1
+        index = 0
+        print('count before', count)
+        while count > 1:
+            print(count, text[index])
+            if text[index] == ' ' and text[index + 1] == ' ':
+                text = text.replace('  ', ' ')
+                par.text = text
+                print(text)
+            count -= 1
+            index += 1
+    print('end')
+
+
+
+
+        # for index, txt in enumerate(text + ' '):
+        #     if text[index] and text[-1:]:
+        #         if text[index] == ' ' and text[index + 1] == " ":
+        #             text = text[index].replace(text[index], '')
+        # par.text = text
 
     doc.save(path_folder + symbol + file_name)
